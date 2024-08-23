314897ea3a4b6b5bfbb4e211353253ff93d47d4b
Nathan <79485616+FACETRU@users.noreply.github.com>
2024-08-23 12:36:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,165 @@
+export const perguntas = [
+    {
+     //numero 1//
+        enunciado: "Se você encontrasse um rato em sua casa, como reagiria?",
+        alternativas: [
+            {
+                texto: " O respeitaria e não o molestaria, considerando-o uma manifestação sagrada.",
+                afirmacao: [
+                    "Respeitar o rato ajudaria a criar uma atmosfera de harmonia com a nova religião, promovendo uma convivência pacífica",
+                    
+                ],
+                proxima: 1,
+            },
+            {
+                texto: " Tentaria afastá-lo ou eliminá-lo, achando que isso é necessário para manter a ordem.",
+
+                afirmacao: [
+                    "Tentar afastá-lo poderia causar conflitos com os seguidores do Ratismo e gerar descontentamento com a nova ordem estabelecida"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+       //numero 2//
+        enunciado: "Como você se sentiria ao participar de uma cerimônia onde ratos são adorados? ",
+        alternativas: [
+            {
+                texto: "  Participaria ativamente, considerando um dever religioso e uma experiência enriquecedora.                ",
+                afirmacao: [
+                    "Participar ativamente fortaleceria sua aceitação da nova religião e integraria você à comunidade.",
+                  
+                ],
+                proxima: 3,
+            },
+            {
+                texto: " Sentiria desconforto e evitava participar, não concordando com a adoração aos ratos.",
+                afirmacao: [
+                    "Sentir desconforto e evitar a participação poderia levar ao isolamento social e à dificuldade de adaptação à nova cultura dominante."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+       //numero 3//
+        enunciado: " Qual seria sua atitude em relação a um templo dedicado aos ratos?",
+        alternativas: [
+            {
+                texto: "  Visitá-lo frequentemente para orações e oferendas, demonstrando devoção.               ",
+                afirmacao: [
+                    "Visitar o templo frequentemente reforçaria seu comprometimento com a fé e fortaleceria suas conexões sociais com outros crentes. ",
+                    
+                ],
+                proxima: 4,
+            },
+            {
+                texto: " Evitaria frequentá-lo, achando que essa prática é estranha e desnecessária.               ",
+                afirmacao: [
+                    " Evitar o templo poderia resultar em um sentimento de alienação e dificuldade em encontrar seu lugar na sociedade religiosa",
+                    
+                ],
+                proxima: 5,
+            },
+        ]
+    },
+    {
+        //numero 4//
+        enunciado: "Como você reagiria se visse alguém oferecendo comida a ratos como parte de um ritual?        ",
+        alternativas: [
+            {
+                texto: "  Veria isso como uma prática normal e aceitável, e talvez até se unisse ao ritual.                ",
+                afirmacao: [
+                    "Ver isso como uma prática normal e até se juntar ao ritual mostraria sua aceitação e integração na nova ordem religiosa."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "  Consideraria essa ação como um comportamento excêntrico e se afastaria?                ",
+                afirmacao: [
+                    "Considerar a ação excêntrica e se afastar poderia criar barreiras entre você e aqueles que seguem as tradições do Ratismo.."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+     //numero 5//   
+        enunciado: "Se fosse solicitado a seguir regras de conduta específicas baseadas no Ratismo, como você se comportaria?        ",
+        alternativas: [
+            {
+                texto: " Cumpriria as regras rigorosamente para estar em conformidade com a nova religião.",
+                afirmacao: [
+                    "Cumprir as regras rigorosamente demonstraria seu respeito pelas normas religiosas e facilitaria sua inclusão no grupo."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: " Desafiaria ou desobedeceria às regras, questionando sua validade e impacto.",
+                afirmacao: [
+                    " Desafiar as regras e questionar sua validade poderia resultar em conflitos e dificultar sua aceitação na nova sociedade."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        //numero 6//
+        enunciado: " Qual seria sua reação ao ver que ratos têm um papel central em decisões políticas ou sociais?        ",
+        alternativas: [
+            {
+                texto: " Aceitaria essa nova ordem como parte da nova realidade e se adaptaria a ela.                ",
+                afirmacao: [
+                    "Aceitar a nova ordem e se adaptar mostraria sua capacidade de integrar-se ao novo sistema e evitar conflitos."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "  Resistiria à mudança e procuraria maneiras de lutar contra a influência dos ratos.                ",
+                afirmacao: [
+                    "Resistir à mudança e lutar contra a influência dos ratos poderia resultar em marginalização e dificuldade em encontrar apoio."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        //numero 7//
+        enunciado: "Como você abordaria a educação das novas gerações sob a influência do Ratismo?        ",
+        alternativas: [
+            {
+                texto: "  Ensinaria as crianças a respeitar e adorar ratos, de acordo com os preceitos da religião?               ",
+                afirmacao: [
+                    "Ensinar as crianças a respeitar e adorar ratos garantiria que a nova geração se alinhasse com os princípios do Ratismo e preservaria a continuidade da religião."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "  Procuraria oferecer uma visão alternativa ou educar as crianças sobre diferentes crenças.    ",
+                afirmacao: [
+                    "Oferecer uma visão alternativa poderia promover um ambiente de debate e resistência, desafiando a homogeneidade religiosa estabelecida."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        //numero 8//
+        enunciado: "Se houvesse um dia sagrado dedicado aos ratos, como você passaria esse dia?        ",
+        alternativas: [
+            {
+                texto: " A) Participaria das celebrações e rituais, conforme recomendado pelos líderes religiosos.                ",
+                afirmacao: [
+                    "Participar das celebrações e rituais indicaria sua conformidade e integração com as práticas religiosas, fortalecendo seus laços comunitários."
+                ],
+            },
+            {
+                texto: " Usaria o dia para atividades pessoais ou para expressar sua opinião contra a religião.",
+                afirmacao: [
+                    "Usar o dia para expressar sua opinião contra a religião poderia levar a um isolamento e a um potencial conflito com os seguidores do Ratismo. "
+                ],
+            },
+        ]
+    }
+];
